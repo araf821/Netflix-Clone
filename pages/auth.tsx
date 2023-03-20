88047aa7565fbd2072b57f2c29da8a9f5657086e
araf821 <araf.ahmed200@gmail.com>
2023-03-19 22:59:03 -0400

    set up sign in section

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -16,13 +18,32 @@ const Auth = () => {
             <h2 className="text-white text-4xl mb-8 font-semibold">Sign In</h2>
             <div className="flex flex-col gap-4">
               <Input
+                id="name"
+                label="Username"
+                value={name}
+                onChange={(e: any) => setName(e.target.value)}
+              />
+
+              <Input
                 id="email"
                 type="email"
-                label="Email address or phone number"
+                label="Email"
                 value={email}
-                onChange={(e: any) => setEmail(e.target.value)} 
+                onChange={(e: any) => setEmail(e.target.value)}
+              />
+
+              <Input
+                id="password"
+                type="password"
+                label="Password"
+                value={password}
+                onChange={(e: any) => setPassword(e.target.value)}
               />
             </div>
+
+            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700   transition">
+              Sign In
+            </button>
           </div>
         </div>
       </div>
