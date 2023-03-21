100265d7745c186a49eac66a1c91133fd6b7ee9c
araf821 <araf.ahmed200@gmail.com>
2023-03-21 01:33:02 -0400

    completed register functionality

--- components/InputBox.tsx
+++ components/InputBox.tsx
@@ -23,7 +23,7 @@ export function InputBox() {
     } catch (error) {
       console.log(error);
     }
-  }, []);
+  }, [email, name, password]);
 
   return (
     <div className="flex justify-center ">
@@ -58,7 +58,10 @@ export function InputBox() {
           />
         </div>
 
-        <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700   transition">
+        <button
+          onClick={register}
+          className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700   transition"
+        >
           {variant === "login" ? "Login" : "Sign Up"}
         </button>
         <p className="text-neutral-500 mt-12">
--- pages/api/register.ts
+++ pages/api/register.ts
@@ -1,4 +1,6 @@
+import bcrypt from "bcrypt";
 import { NextApiRequest, NextApiResponse } from "next";
+import prismadb from "@/lib/prismadb";
 
 export default async function handler(
   req: NextApiRequest,
@@ -10,6 +12,32 @@ export default async function handler(
 
   try {
     const { email, name, password } = req.body;
+
+    const user = await prismadb.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (user) {
+      return res
+        .status(402)
+        .json({ error: "An account with this email already exists." });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const newUser = await prismadb.user.create({
+      data: {
+        email,
+        name,
+        hashedPassword,
+        image: "",
+        emailVerified: new Date(),
+      },
+    });
+
+    return res.status(200).json(newUser);
   } catch (error) {
     console.log(error);
     return res.status(400).end();
