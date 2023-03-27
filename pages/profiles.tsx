7f2ba0461d6e217c4023d8332e38d3c045b19994
araf821 <araf.ahmed200@gmail.com>
2023-03-26 23:50:53 -0400

    complete profiles page

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -1,5 +1,7 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -18,7 +20,10 @@ export async function getServerSideProps(context: NextPageContext) {
   };
 }
 
-const profiles = () => {
+const Profiles = () => {
+  const router = useRouter();
+  const { data: user } = useCurrentUser();
+
   return (
     <div className="flex items-center h-full justify-center">
       <div className="flex flex-col">
@@ -26,7 +31,7 @@ const profiles = () => {
           Who is watching?
         </h1>
         <div className="flex items-center justify-center gap-8 mt-10">
-          <div onClick={() => {}}>
+          <div onClick={() => router.push("/")}>
             <div className="group flex-row w-44 mx-auto">
               <div
                 className="
@@ -45,6 +50,17 @@ const profiles = () => {
               >
                 <img src="/images/red-profile.png" alt="" />
               </div>
+              <div
+                className="
+                mt-4
+                text-gray-400
+                text-2xl
+                text-center
+                group-hover:text-white
+              "
+              >
+                {user?.name}
+              </div>
             </div>
           </div>
         </div>
@@ -53,4 +69,4 @@ const profiles = () => {
     </div>
   );
 };
-export default profiles;
+export default Profiles;
