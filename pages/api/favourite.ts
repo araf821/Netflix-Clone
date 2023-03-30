ab59163e45b1e2010c5c7dc4a3198fa841d2b9a5
araf821 <araf.ahmed200@gmail.com>
2023-03-30 01:07:06 -0400

    implemented functionality for deleting from favourites list

--- pages/api/favourite.ts
+++ pages/api/favourite.ts
@@ -1,5 +1,6 @@
 import prismadb from "@/lib/prismadb";
 import serverAuth from "@/lib/serverAuth";
+import { without } from "lodash";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
@@ -33,6 +34,36 @@ export default async function handler(
 
       res.status(200).json(user);
     }
+
+    if (req.method === "DELETE") {
+      const { currentUser } = await serverAuth(req);
+      const { movieId } = req.body;
+
+      const existingMovie = await prismadb.movie.findUnique({
+        where: {
+          id: movieId,
+        },
+      });
+
+      if (!existingMovie) {
+        throw new Error("Invalid ID.");
+      }
+
+      const updatedMovieList = without(currentUser.favouriteIds, movieId);
+
+      const updatedUser = await prismadb.user.update({
+        where: {
+          email: currentUser.email || "",
+        },
+        data: {
+          favouriteIds: updatedMovieList,
+        },
+      });
+
+      return res.status(200).json(updatedUser);
+    }
+
+    return res.status(405).end();
   } catch (error) {
     console.log(error);
     res.status(400).end();
